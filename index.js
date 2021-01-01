d6dbe4ffc69ae492f5d69fe6fe7fe04be74d5f02
Shubh V Jain <shubhvjain@gmail.com>
2021-01-01 13:29:58 +0530

    ...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 class mGraph {
     constructor(graphName, options = {}) {
-        let requiredOptions = ["dbURL","dbName","collection"]
+        let requiredOptions = ["dbURL", "dbName", "collection"]
         let validOptions = []
         this.graphName = graphName
         if (!options.dbURL) { throw new Error("Specify MongoDB URL") }
@@ -15,38 +15,38 @@ class mGraph {
         this.collection = options.collection
 
         // config for  simple unidirected graph
-        this.graph={}
-        this.graph.directed=false
+        this.graph = {}
+        this.graph.directed = false
         this.graph.multiEdges = false
         this.graph.loops = false
 
-        console.log("Graph type : "+this.getGraphType())
+        console.log("Graph type : " + this.getGraphType())
         console.log("Starting connection.....")
-
-        let obj = this
         MongoClient.connect(this.dbURL, { useUnifiedTopology: true }, function (err, client) {
             console.log("....connected successfully to server")
-            obj.db = client.db(obj.dbName);
-            obj.connect = true;
         });
     }
-    getGraphType(){
-        let graphType=""
-        graphType += this.graph.multiEdges? "muti" : "simple"
-        graphType += this.graph.directed? "-directed ":"-undirected "
-        graphType += this.graph.loops? "-withLoops":""
+    getGraphType() {
+        let graphType = ""
+        graphType += this.graph.multiEdges ? "muti" : "simple"
+        graphType += this.graph.directed ? "-directed " : "-undirected "
+        graphType += this.graph.loops ? "-withLoops" : ""
         return graphType
     }
+    
 }
 
-
-try {
-    let graph1 = new mGraph("roadmap",{ 
-        dbURL: process.env.MDB, 
-        dbName: "graph1",
-        collection:"knowledge1"
-    })
-} catch (error) {
-    console.log("error.....")
-    console.log(error)
+ main = async ()=>{
+    try {
+        let graph1 = new mGraph("roadmap", {
+            dbURL: process.env.MDB,
+            dbName: "graph1",
+            collection: "knowledge1"
+        })
+    } catch (error) {
+        console.log("error.....")
+        console.log(error)
+    }    
 }
+
+main()
